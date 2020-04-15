a8b9135e3f923032b696a8f51c7f8d3b91c3326e
RadenAR <radenabdulrahman@gmail.com>
2020-04-15 18:51:57 -0400

    Ability to load photos

--- Fiftygram/ViewController.swift
+++ Fiftygram/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let context = CIContext()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBAction func choosePhoto() {
+        if  UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            navigationController?.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func applySepia() {
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        filter?.setValue(CIImage(image: imageView.image!), forKey: kCIInputImageKey)
+        let output = filter?.outputImage
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output!, from: output!.extent)!)
     }
 
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        navigationController?.dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+        }
+    }
 }
 
